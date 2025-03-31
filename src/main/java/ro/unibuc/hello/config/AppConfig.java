9847d8a6ba699d125ae28383e082a6998d73c036
diaconescualexandra <116594293+diaconescualexandra@users.noreply.github.com>
2025-03-31 21:51:34 +0300

    setup e2e testing for ride booking (#18)
    * setup e2e testing for ride booking
    
    * add ride booking e2e tests

--- src/main/java/ro/unibuc/hello/config/AppConfig.java
+++ src/main/java/ro/unibuc/hello/config/AppConfig.java
@@ -6,6 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.time.Clock;
+
 @Configuration
 public class AppConfig {
 
@@ -18,4 +24,13 @@ public class AppConfig {
     public Clock clock() {
         return Clock.systemDefaultZone();
     }
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return objectMapper;
+    }
+
 }
+
--- src/main/java/ro/unibuc/hello/controller/GreetingsController.java
+++ src/main/java/ro/unibuc/hello/controller/GreetingsController.java
@@ -2,9 +2,6 @@ package ro.unibuc.hello.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import ro.unibuc.hello.dto.Greeting;
 import ro.unibuc.hello.exception.EntityNotFoundException;
@@ -46,13 +43,13 @@ public class GreetingsController {
 
     @PutMapping("/greetings/{id}")
     @ResponseBody
-    public Greeting updateGreeting(@PathVariable String id, @RequestBody Greeting greeting) throws EntityNotFoundException {
+    public Greeting updateGreeting(@PathVariable("id") String id, @RequestBody Greeting greeting) throws EntityNotFoundException {
         return greetingsService.updateGreeting(id, greeting);
     }
 
     @DeleteMapping("/greetings/{id}")
     @ResponseBody
-    public void deleteGreeting(@PathVariable String id) throws EntityNotFoundException {
+    public void deleteGreeting(@PathVariable("id") String id) throws EntityNotFoundException {
         greetingsService.deleteGreeting(id);
     }
 }
